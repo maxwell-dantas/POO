de73aaf81acae21ba64bbe394263c58a03f9acd2
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-03-30 18:44:44 +0000

    feat: simulação de saque e depósito (Q3)

---
+++ src/Lista02/Q3.java
@@ -0,0 +1,67 @@
+package Lista02;
+import java.util.Scanner;
+
+/* Escrever uma classe para modelar uma conta bancária com todos os atributos encapsulados. A classe deve ter
+* atributos para armazenar o nome do titular da conta, o número da conta e seu saldo e métodos para realizar as
+* operações de depósito e saque, além dos métodos de acesso para definir e recuperar os atributos.
+* Escrever um programa para testar a classe e elaborar seu digrama UML. */
+
+class ContaBancaria {
+    private String nomeTitular;
+    private String numeroConta;
+    private double saldo;
+
+    public ContaBancaria(String nomeTitular, String numeroConta, double saldo) {
+        this.nomeTitular = nomeTitular;
+        this.numeroConta = numeroConta;
+        this.setSaldo(saldo);
+    }
+
+    public void setSaldo(double saldo) {
+        if (saldo >= 0) {
+            this.saldo = saldo;
+        } else throw new IllegalArgumentException("Insira um saldo válido!");
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void deposito(double valorDeposito) {
+        this.saldo += valorDeposito;
+    }
+
+    public void saque(double valorSaque) {
+        if (this.saldo >= valorSaque) {
+            this.saldo -= valorSaque;
+        } else throw new IllegalArgumentException("Saldo insuficiente!");
+    }
+
+    public String toString() {
+        return "--------------------\nNome do titular da conta: " + this.nomeTitular + "\nNúmero da conta: " + this.numeroConta + "\nSaldo atual da conta: R$" + this.saldo + "\n--------------------";
+    }
+}
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Insira o nome do titular da conta: ");
+        String nomeTitular = leitor.nextLine();
+
+        System.out.print("Insira o número da conta: ");
+        String numeroConta = leitor.nextLine();
+        
+        System.out.print("Insira o saldo atual da conta: ");
+        double saldoConta = leitor.nextDouble();
+        
+        ContaBancaria conta01 = new ContaBancaria(nomeTitular, numeroConta, saldoConta);
+        
+        System.out.println(conta01);
+        conta01.deposito(300);
+        System.out.println(conta01);
+        conta01.saque(400);
+        System.out.println(conta01);
+        leitor.close();
+    }
+}
